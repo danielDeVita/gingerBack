b890eb55b9cf821d5e0e6348b2a21a74f7e6b176
danielDeVita <danielitodevita@gmail.com>
2023-05-02 16:37:47 -0300

    db_name env

--- src/database.ts
+++ src/database.ts
@@ -6,10 +6,11 @@ dotenv.config();
 const DB_USER: string = process.env.DB_USER!;
 const DB_PASSWORD: string = process.env.DB_PASSWORD!;
 const CLUSTER: string = process.env.CLUSTER!;
+const DB_NAME: string = process.env.DB_NAME!;
 
 export default async function connect(): Promise<void> {
     try {
-        await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${CLUSTER}.mongodb.net`, {
+        await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${CLUSTER}.mongodb.net/${DB_NAME}`, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         } as mongoose.ConnectOptions);
